0c18807e5dd7ddd8fef43a9b2f9a58f532904588
Aamer2005 <2023bit064@sggs.ac.in>
2025-08-03 14:15:12 +0530

    I learned how to get the input using sehll script

---
+++ ShellInput.sh
@@ -0,0 +1,13 @@
+echo "Hello , I want to know who am i talking to ? "
+
+read name
+
+echo "It\'s Nice To Meet You $name"
+
+read -p "Enter UserName" username
+read -sp "Enter Password" password
+
+echo 
+echo "Information : Username : $username Password : $password"
+
+
